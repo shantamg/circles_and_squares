584f3b416c1156bcd61d8084f0a2857e12250374
Jason Galuten <jason@galuten.com>
2016-06-12 19:02:32 -0700

    default to not invert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_invert
 
   def set_invert
-    session[:invert] = 'invert' unless session[:invert]
+    session[:invert] = '' unless session[:invert]
   end
 end
