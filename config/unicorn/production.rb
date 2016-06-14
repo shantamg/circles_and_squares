eba3abbaba029af59936cde3ebeec895d8522ea3
Jason Galuten <jason@galuten.com>
2016-06-14 12:08:59 -0700

    tweaking unicorn settings

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,3 +7,29 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.circlez_and_squarez.sock"
 worker_processes 2
 timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+end
